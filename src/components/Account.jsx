bcb6d13490c98d08d2adc4e64edca78a9c601098
monikabaltova <monika.baltova31@gmail.com>
2025-09-13 18:43:29 +0100

    refactor my account/ created alert

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const MyAccount = ({ userId, user, setUser }) => {
+  if (!user?.user_id) {
+    alert("Please log in into your account!");
+    return;
+  }
   useEffect(() => {
     axios
       .get(`https://airbnc-1dqu.onrender.com/api/users/${userId}`)
